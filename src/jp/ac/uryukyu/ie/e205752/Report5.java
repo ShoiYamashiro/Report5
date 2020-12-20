a4c75427b416efbe7d88c6cc67989736acc598d3
Shoi Yamashiro <e205752@ie.u-ryukyu.ac.jp>
2020-12-20 15:33:23 +0900

    ステップ3

--- src/jp/ac/uryukyu/ie/e205752/Report5.java
+++ src/jp/ac/uryukyu/ie/e205752/Report5.java
@@ -1,7 +1,7 @@
 package src.jp.ac.uryukyu.ie.e205752;
 import java.io.FileWriter;
 import java.io.IOException;
-
+//ステップ３
 public class Report5 {
     public static void main(String[] args){
        String str ="百二十三";
