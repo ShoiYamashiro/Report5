b922e529dd78ccc8faf8b6e741a03a58da6bbcf9
Shoi Yamashiro <e205752@ie.u-ryukyu.ac.jp>
2020-12-20 15:30:49 +0900

    ステップ

--- src/jp/ac/uryukyu/ie/e205752/Report5.java
+++ src/jp/ac/uryukyu/ie/e205752/Report5.java
@@ -4,17 +4,7 @@ import java.io.IOException;
 
 public class Report5 {
     public static void main(String[] args){
-        try{
-            // FileWriter fw = new FileWriter("output.txt");
-            String str = null;
-            System.out.println(str.length());
-            // fw.close();
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生");
-            // }catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
+       String str ="百二十三";
+       int number = Integer.parseInt(str);
     }
 }
