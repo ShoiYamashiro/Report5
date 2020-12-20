bc1ec833549432cc2e0a31191a9bdffd03105d32
Shoi Yamashiro <e205752@ie.u-ryukyu.ac.jp>
2020-12-20 14:53:46 +0900

    仕様条件１

---
+++ src/jp/ac/uryukyu/ie/e205752/Report5.java
@@ -0,0 +1,12 @@
+package src.jp.ac.uryukyu.ie.e205752;
+
+public class Report5 {
+    public static void main(String[] args){
+
+         String str = null;
+         int num = str.length();
+
+         System.out.println(num);
+
+     }
+}
